3de3999b1a84565011cfbb6416c53ddc90964cec
victorizbitskiy <victorizbitskiy@yandex.ru>
2021-10-01 18:36:18 +0300

    added prototype.js

---
+++ 01_prototype.js
@@ -0,0 +1,18 @@
+const person = new Object({
+  name: 'Ivan',
+  age: 30,
+  greet: function() {
+    console.log('Greet!');
+  }
+})
+
+Object.prototype.sayHallo = function() {
+  console.log('Hello!')
+}
+
+// Делаем person прототипом 
+const masha = Object.create(person) 
+masha.name = 'Mary'
+
+// const str = `I'll be back `
+const str = new String(`I'll be back `)
