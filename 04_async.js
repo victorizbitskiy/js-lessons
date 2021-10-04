3042720431cdfe563d6ef150fa4eca471ebb8207
victorizbitskiy <62376906+victorizbitskiy@users.noreply.github.com>
2021-10-04 12:20:27 +0300

    Update 04_async.js

--- 04_async.js
+++ 04_async.js
@@ -1,10 +1,10 @@
-console.log('Start 1')
+console.log('Start 1') // будет выполнен синхронно
 
 setTimeout(function() {
-  console.log('Inside timeout, after 5 seconds')
+  console.log('Inside timeout, after 5 seconds') // будет выполнен асинхронно
 }, 5000)
 
-console.log('Start 2')
+console.log('Start 2') // будет выполнен синхронно
 
 // Event loop example
 // http://latentflip.com/loupe
