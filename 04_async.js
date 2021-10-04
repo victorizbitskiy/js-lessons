7a441b860a4355d022b9061e2504bc7ad4ef0093
victorizbitskiy <victorizbitskiy@yandex.ru>
2021-10-04 10:00:09 +0300

    added 04_async.js

---
+++ 04_async.js
@@ -0,0 +1,10 @@
+console.log('Start 1')
+
+setTimeout(function() {
+  console.log('Inside timeout, after 5 seconds')
+}, 5000)
+
+console.log('Start 2')
+
+// Event loop example
+// http://latentflip.com/loupe
