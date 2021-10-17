c451ef0cc37f7588cea59fa7b48506b16400c16b
victorizbitskiy <victorizbitskiy@yandex.ru>
2021-10-17 20:18:34 +0300

    added 17.localStorage.js

---
+++ 17_localStorage.js
@@ -0,0 +1,49 @@
+const myNumber = 42
+// localStorage.removeItem('number')
+// localStorage.setItem('number', myNumber.toString())
+// console.log(localStorage.getItem('number'))
+// localStorage.clear()
+
+const object = {
+  name: 'Max',
+  age: 20
+}
+
+// localStorage.setItem('person', object) // в value будет [object Object], т.к. у объекта вызовется toString()
+
+// Запись объекта в localStorage
+localStorage.setItem('person', JSON.stringify(object)) // в value будет строка в формате Json {"name":"Max","age":20}
+
+const raw = localStorage.getItem('person')
+// raw.name = 'Victor' // не сработает
+
+// console.log(typeof raw) // string
+
+// Нужно сделать так:
+// console.log(JSON.parse(raw))
+
+const person = JSON.parse(raw)
+person.name = 'Victor'
+
+console.log(person)
+// ====
+
+// Особенности работы с localStorage
+// Если приложение открыто в разных вкладках, то можно это синхронизировать
+
+// 1 способ синхронизации
+// Событие происходит тогда, когда что-то записывается в localStorage на другой вкладке
+window.addEventListener('storage', event => {
+  console.log(event)
+})
+
+// 2 способ синхронизации
+// window.onstorage = () => {}
+
+// Отличия localStorage отличается от cookies
+
+// 1) localStorage намного больше. Обычно это около 5 Мб
+// 2) cookies улетают на сервер. Это не безопасно.
+
+
+
